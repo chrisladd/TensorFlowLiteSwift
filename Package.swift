ad564f99555cd29f799ea78727a4fb7d8a438fb4
Chris Ladd <c.g.ladd@gmail.com>
2023-03-25 14:53:56 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["TensorFlowLite"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kewlbear/TensorFlowLiteC.git", .branch("master")),
+        .package(url: "https://github.com/kewlbear/TensorFlowLiteC.git", from: "0.0.1")),
     ],
     targets: [
         .target(
